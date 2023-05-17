b8b790e62daaafbcb45acadb6db260a90d3a52b4
haris494 <hariis.459@gmail.com>
2023-05-17 19:27:42 +0700

    praktikum9

--- Praktikum9/Praktikum9.cpp
+++ Praktikum9/Praktikum9.cpp
@@ -9,6 +9,34 @@ public:
 	string nama;
 
 	void setID();
-	void printALL();
+	void printAll();
 	mahasiswa(string pnama) :nama(pnama) { setID(); }
-}; 
+};
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;
+} 
+
+void mahasiswa::printAll() {
+	cout << "ID   = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << "NIM = " << nim << endl;
+	cout << endl;
+} 
+
+int main() {
+	mahasiswa mhs1("lia kurnia");
+	mahasiswa mhs2("Asroni");
+	mhs2.nim = 10;
+	mahasiswa mhs3("Andi Kurniawan");
+	mhs3.nim = 20;
+	mahasiswa mhs4("Joko Purbo");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	return 0;
+} 
